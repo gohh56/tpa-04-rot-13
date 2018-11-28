070b82f9ad2d3d3e6b0b21ffb6850cd1eaff0bca
gohh56 <ystokio@gmail.com>
2018-11-29 02:22:04 +0900

    fix Rot13

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
-const rot13Input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-const rot13Output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split('');
+const rot13Input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const rot13Output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
 
-const rotateChar = function(ch) {
-  return rot13Input.indexOf(ch) > -1 ? rot13Output[rot13Input.indexOf(ch)] : ch;
+const rotateChar = function(char) {
+  const rotIndex = rot13Input.indexOf(char);
+  return rotIndex !== -1 ? rot13Output[rotIndex] : char;
 };
 
 const convertRot13 = function(sourceText) {
@@ -10,18 +11,18 @@ const convertRot13 = function(sourceText) {
 };
 
 const handleBtnConvert = function() {
-  const sourceText = document.querySelector('#container .source-text').value;
+  const sourceText = document.querySelector('.source-text').value;
   const convertedText = convertRot13(sourceText);
-  document.querySelector('#container .destination-text').value = convertedText;
+  document.querySelector('.destination-text').value = convertedText;
 };
 
 const initPage = function() {
-  const form = document.querySelector('#container .form-convert');
+  const form = document.querySelector('.form-convert');
   // see https://developer.mozilla.org/ja/docs/Web/API/Event/preventDefault
   form.addEventListener('submit', evt => evt.preventDefault());
 
-  document.querySelector('.btn-convert')
-    .addEventListener('click', handleBtnConvert);
+  const btn = document.querySelector('.btn-convert');
+  btn.addEventListener('click', handleBtnConvert);
 };
 
 initPage();
